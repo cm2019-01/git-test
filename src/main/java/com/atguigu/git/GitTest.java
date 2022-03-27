e81bd619becd2e9f9e522d6f523aaefa689fba7f
jakechen <jakechen@qq.com>
2022-03-27 18:57:36 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git = " + "hello git");
         System.out.println("hello git2= " + "hello git2");
+        System.out.println("hello git3= " + "hello git3");
     }
 }
