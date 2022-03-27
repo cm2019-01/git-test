a4440497a6934f2001e9f836fa8cd0c5c30b80e8
jakechen <jakechen@qq.com>
2022-03-27 18:42:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName:GitTest
+ * @Author:admin
+ * @Date;2022/3/27 18:21
+ * @Version V1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git = " + "hello git");
+        System.out.println("hello git = " + "hello git");
+    }
+}
---
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName:GitTest2
+ * @Author:admin
+ * @Date;2022/3/27 18:26
+ * @Version V1.0
+ **/
+public class GitTest2 {
+
+}
